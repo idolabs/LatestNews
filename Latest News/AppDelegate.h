7399a48cfb58809b261c6fff4e1e0bfff6057275
Inanc Sevinc <inancsevinc@Inancs-MacBook-Pro.local>
2012-09-01 17:32:27 +0300

    Initial Commit

---
+++ Latest News/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Latest News
+//
+//  Created by Inanc Sevinc on 9/1/12.
+//  Copyright (c) 2012 idolabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Latest News/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Latest News
+//
+//  Created by Inanc Sevinc on 9/1/12.
+//  Copyright (c) 2012 idolabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
