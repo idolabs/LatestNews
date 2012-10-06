77f33e8d9a983ed3792aa12b2c03a4fe4571c1db
Bahaddin Yasar <bahaddinyasar@gmail.com>
2012-10-06 18:41:25 +0300

    settings table view controller

---
+++ Latest News/Classes/Controller/SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  Latest News
+//
+//  Created by Mehmet Bahaddin Yasar on 10/6/12.
+//  Copyright (c) 2012 idolabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController <UITableViewDelegate>
+
+@end
